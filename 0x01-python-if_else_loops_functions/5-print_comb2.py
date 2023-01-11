71506f14ca9f5eb398075296c238804d214f61f3
Onyema <onyema.bosworth@gmail.com>
2023-01-11 02:11:39 -0800

    new3

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
-n range(0, 100):
-        if number == 99:
-                    print("{}".format(number))
-                        else:
-                                    print("{:02}".format(number), end=", ")
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
