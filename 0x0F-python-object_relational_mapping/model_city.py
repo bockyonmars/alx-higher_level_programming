112445d65889df461c8c1225d867e45528c015a0
Bosworth Onyema <onyema.bosworth@gmail.com>
2024-01-21 14:25:51 +0000

    l

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,4 +1,6 @@
-ontains the class definition of a City
+#!/usr/bin/python3
+"""
+Contains the class definition of a City
 """
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,4 +1,6 @@
-ontains the class definition of a City
+#!/usr/bin/python3
+"""
+Contains the class definition of a City
 """
 from relationship_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
